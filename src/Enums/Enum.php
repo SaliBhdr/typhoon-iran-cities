a25f51ef3a96a0ecf672f16ab569aa5498e9753c
Iman Parvizi <70389233+realpvz@users.noreply.github.com>
2021-09-19 00:16:52 +0430

    get array of constants
    i think its better way to get array of final classe's toArray

---
+++ src/Enums/Enum.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Constants;
+
+use League\Flysystem\NotSupportedException;
+use ReflectionClass;
+
+/**
+ * Class Gender
+ *
+ * @package \App\Constants
+ */
+abstract class Enum
+{
+
+
+    /**
+     * Enum constructor.
+     */
+     private function __construct()
+    {
+        throw new NotSupportedException(); //
+    }
+
+    /**
+     *
+     */
+     private function __clone()
+    {
+        throw new NotSupportedException();
+    }
+
+    /**
+     * @return array
+     */
+     public static function toArray(): array
+    {
+        return (new ReflectionClass(static::class))->getConstants();
+    }
+
+    /**
+     * @param $value
+     *
+     * @return bool
+     */
+     public static function isValid($value): bool
+    {
+        return in_array($value, static::toArray());
+    }
+
+}
